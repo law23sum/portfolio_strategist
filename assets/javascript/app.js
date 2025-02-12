57b0bd990f7ea0d07579a6e7aba386db742b868f
law23sum <94133747+law23sum@users.noreply.github.com>
2025-02-12 09:33:10 +0000

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+import * as JsCookie from "js-cookie"; // generated
+export { Modals } from './web/modals';
+export { DashboardCharts } from './dashboard/dashboard-charts';
+
+// pass-through for Cookies API
+export const Cookies = JsCookie.default;
